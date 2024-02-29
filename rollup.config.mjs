883360b2d2ba636aa2c845b8cb23c0836a2c2e13
imndx <imndxx@gmail.com>
2024-03-01 00:04:39 +0800

    fix build error

---
+++ rollup.config.mjs
@@ -0,0 +1,77 @@
+import pkg from './package.json' assert { type: "json" };
+import resolve from '@rollup/plugin-node-resolve';
+import rpt2 from 'rollup-plugin-typescript2';
+import vue from 'rollup-plugin-vue';
+import css from 'rollup-plugin-css-porter';
+import filesize from 'rollup-plugin-filesize';
+import typescript from 'typescript'
+
+// Default configs
+const name = 'VEmojiPicker';
+const exports = 'named';
+const sourcemap = false;
+const globals = {
+};
+
+export default {
+  input: 'src/index.ts', // our source file
+  output: [
+    {
+      // Keep the bundle as an ES module file, suitable for other bundlers
+      // and inclusion as a <script type=module> tag in modern browsers
+      name,
+      file: pkg.module,
+      format: 'esm', // the preferred format
+      compact: true,
+      exports,
+      sourcemap
+    },
+    {
+      // A self-executing function, suitable for inclusion as a <script> tag.
+      // (If you want to create a bundle for your application, you probably want to use this.)
+      name,
+      file: pkg.unpkg,
+      format: 'iife',
+      compact: true,
+      exports,
+      sourcemap,
+      globals
+    },
+    {
+      // CommonJS, suitable for Node and other bundlers
+      name,
+      file: pkg.main,
+      format: 'cjs',
+      compact: true,
+      exports,
+      sourcemap,
+      globals
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies)
+  ],
+  plugins: [
+    rpt2({
+      typescript: typescript,
+      module: 'esnext',
+      tsconfig: "tsconfig.json",
+      check: false,
+      rollupCommonJSResolveHack: false,
+      tsconfigOverride: { exclude: ["node_modules", "src/main.ts", "tests"] }
+    }),
+    css(),
+    vue({
+      css: true,
+      template: {
+        isProduction: true
+      }
+    }),
+    resolve({
+      extensions: ['.js', '.ts']
+    }),
+    filesize({
+      showBrotliSize: true
+    }),
+  ]
+};
